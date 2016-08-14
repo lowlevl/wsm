d33a4c5f2e7a7b895095953da1c785a764343579
Thecakeisgit <Thecakeisplaying@gmail.com>
2016-08-14 16:26:03 +0200

    Filled default page

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Default Page</title>
+  </head>
+  <body>
+    <h1>This is the default page of this webserver,</h1><br>
+    For further information please check <a href="https://github.com/Thecakeisgit/securhOst">the project page :D</a>
+    <br>Nota bene: If you are a customer, please contact your webadmin to fix this ugly default page ;D
+  </body>
+</html>
