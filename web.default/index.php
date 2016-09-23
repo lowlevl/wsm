30727fcc494ec3cc789d7c6dc3fcbc07f7f8cd50
Thecakeisgit <Thecakeisplaying@gmail.com>
2016-09-23 13:48:01 +0200

    Modified default page for a better look

--- web.default/index.php
+++ web.default/index.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
-    <title>Default Page</title>
+    <meta charset="utf-8">
+    <title>Default Page</title> 
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   </head>
   <body>
-    <h1>This is the default page of this webserver,</h1><br>
-    For further information please check <a href="https://github.com/Thecakeisgit/hostmgr">the project page :D</a>
-    <br>Nota bene: If you are a customer, please contact your webadmin to fix this ugly default page ;D
+    <div align="center">
+        <h1>This is the default page of this webserver,</h1><br>
+        For further information please check <a href="https://github.com/Thecakeisgit/hostmgr">the project page :D <i class="glyphicon glyphicon-heart"></i></a>
+        <br>Nota bene: If you are a customer, please contact your webadmin to fix this ugly default page ;D
+      </div>
   </body>
 </html>
