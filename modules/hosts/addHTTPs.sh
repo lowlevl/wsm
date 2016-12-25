44160a4c82d41e3866dd1a8b2c23b0f537907efe
Thecakeisgit <Thecakeisplaying@gmail.com>
2016-12-25 17:24:16 +0100

    Little unnatended fix for if..Then..else..

--- modules/hosts/addHTTPs.sh
+++ modules/hosts/addHTTPs.sh
@@ -27,7 +27,7 @@ service apache2 reload
 echo "Ok."
 
 echo "   *** Generating certificate ***"
-if [ ! -z "$serverAlias" ] # Check if aliases; then
+if [ ! -z "$serverAlias" ]; then # Check if aliases
   $letsEncryptExec certonly --webroot --webroot-path "$serverDir/www/" -d "$serverName" -d "$serverAlias"
 else
   $letsEncryptExec certonly --webroot --webroot-path "$serverDir/www/" -d "$serverName"
